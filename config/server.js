5d78af5d451805be955593083def9ca026d3699b
arslanahmadvirk <arslanahmadv098@gmail.com>
2021-12-02 17:05:02 +0500

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '046b775452ee107e35f2e6ff631b1983'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f0c5b353-de3e-4ae5-8511-c943a7b0ae24'
+};
